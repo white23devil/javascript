ea2871c4fd2b0d02f04d698241514f1f1d7344fd
abhivandit sharma <118964482+white23devil@users.noreply.github.com>
2024-08-22 09:16:22 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId =  144553  //constant ko declare kre ka ek hi trika hai const keword lgake
+let accountEmail = "Abhivanditsharma@gmail.com"
+var accountPassword = "12345"//variable ko 2 trike se declare kr skte hai , ek "let" and dusra"var", but we dont use var because of scope so we use var.
+accountancy = "Jaipur" //possibility hai ki bina declare ke aap variable declare kr skte ho.
+
+// accountId = 2;
+
+accountEmail = "abhaysharma@gmail.com"
+accountPassword = "232342"
+accountancy = "Amritsar"
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountancy);
+
+console.table([accountId,accountEmail,accountPassword,accountancy])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+//pure code ko new code ki trha treat kro uske liye hum "use strict" kregei
+"use strict";
+// alert("hello")  //we are using node.js not a browser
+
+let name = "Abhivandit"
+let age = 18
+let isLoggedIn = false
+
+//numbers
+//bigint
+//string
+//boolean
+//null
+//undefined
+//symbol
+//object
+
+console.log(typeof undefined) //undefined ka output undefined hi ata hai
+console.log(typeof null) //Null ka output object ata hai
