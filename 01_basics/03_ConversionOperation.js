c8aa8d972fa7e95928db0fdc266fccd474980915
abhivandit sharma <118964482+white23devil@users.noreply.github.com>
2024-08-22 09:46:55 +0000

    conversion of datatypes

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,45 @@
+/*let score = "33abc";       //string
+console.log(typeof score)
+
+let valueInNumber = Number(score)  //type casting ki hai
+
+console.log(typeof valueInNumber)   
+
+console.log(score)
+console.log(valueInNumber)  //NaN is not a number, which is special type
+
+console.log("let check for NULL")
+
+let age = null;       //string
+console.log(typeof age)
+
+let value = Number(score)  //type casting ki hai
+
+console.log(typeof value)   
+
+console.log(age)   
+console.log(value) 
+*/
+
+//"33" => 33
+//"33abc" => NaN  lekin iski type number hi hai
+//true => 1
+//false => 0
+
+//let isLoggedIn = 1
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn)
+
+//1 => true
+//0 =>false
+
+//"" =>false
+//"Abhivandit" =>true 
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
