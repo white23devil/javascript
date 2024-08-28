1c66297094c3852ae1e0ab2cc74bb562eb4f6564
abhivandit sharma <118964482+white23devil@users.noreply.github.com>
2024-08-28 15:23:10 +0000

    objects and json

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,31 @@
+//API
+const course = {
+    coursename : "js in hindi",
+    price: "999",
+    courseInstructor: "Abhivandit"
+}
+
+// console.log(course.coursename)
+
+const {courseInstructor} = course  // destructuring
+
+console.log(courseInstructor);
+
+//What is API's?
+// Sidhi baat : Jb bhi apko apna kaam kisi aur ke ser pr dalna ho
+
+//JSON
+// simple keys and values both are in string
+
+// {
+//     "name" : "Abhay",
+//     "coursename" : "Js in hindi",
+//     "prince" : "free"
+
+// }
+
+[
+    {},
+    {},
+    {}
+]
