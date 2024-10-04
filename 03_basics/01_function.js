13d1056153fd67bf9f39b3c60e4d74e997c0d981
abhivandit sharma <118964482+white23devil@users.noreply.github.com>
2024-10-04 19:04:55 +0000

    basic of function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -4,5 +4,34 @@
 function func_name(){
     //Code
 }
-*/
 
+
+func_name()
+
+*/
+function name(){
+    console.log("Abhivandit");
+}
+
+// name();
+
+function add(num1 , num2){
+    return(num1+num2);
+}
+
+// const result = add(3,2);
+// console.log(result)
+
+function userLogIn(username = "User"){  // yaki user kbhi bhi undefined name na pass kre
+    if(username == undefined){
+        console.log("Please Enter the username");
+        return;
+    }
+    else{
+        return `${username} just logged in`   //string manipulation
+    }
+    
+}
+
+// console.log(userLogIn("Abhivandit"));
+console.log(userLogIn());
